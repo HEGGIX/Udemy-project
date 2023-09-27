90bbcef02145ddc3a6888fcdad23ee752b385edf
HEGGIX <glebermakovich@gmail.com>
2023-09-27 17:01:57 +0300

    fix: add Films

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+let nameOfFilms = prompt("Один из последних просмотренных фильмов?",""),
+    gradeOfFilms = prompt("Какую оценку ему поставите?", ""),
+    nameOfFilms2 = prompt("Один из последних просмотренных фильмов?",""),
+    gradeOfFilms2 = prompt("Какую оценку ему поставите?", "");
+personalMovieDB.movies[nameOfFilms] = gradeOfFilms;
+personalMovieDB.movies[nameOfFilms2] = gradeOfFilms2;
+console.log(personalMovieDB);
